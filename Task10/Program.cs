baef8bb0c4f1a783eb39c641a597470b80aebfe3
Yar_Sanych_NB <y.krivoff@gmail.com>
2022-08-23 15:26:39 +0300

    Код для Task10 готов!

---
+++ Task10/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+// Example:
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+
+int SecondDigit(int number)
+{
+    int dig = (number / 10) % 10;
+    return dig;
+}
+
+
+Console.Write("Введите любое положительное трёхзначное число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("");
+
+Console.WriteLine($"Вторая цифра числа {num} --> {SecondDigit(num)}");
+Console.WriteLine("");
